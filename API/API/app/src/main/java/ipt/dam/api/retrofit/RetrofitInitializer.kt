4feff619097cb99f9691ce8cfc803cbcdec46eb1
pauloagsantos <pauloagsantos@gmail.com>
2023-11-03 18:11:42 +0000

    codigo feito nas aulas

--- API/API/app/src/main/java/ipt/dam/api/retrofit/RetrofitInitializer.kt
+++ API/API/app/src/main/java/ipt/dam/api/retrofit/RetrofitInitializer.kt
@@ -9,10 +9,13 @@ import retrofit2.converter.gson.GsonConverterFactory
 class RetrofitInitializer {
 
     private val gson: Gson = GsonBuilder().setLenient().create()
+    private val host = "http://ram.ipt.pt/"
+    //private val host = "http://10.0.2.2/"
+
+
 
     private val retrofit = Retrofit.Builder()
-        //.baseUrl("http://10.0.2.2/")
-        .baseUrl("http://ram.ipt.pt/")
+        .baseUrl(host)
         .addConverterFactory(GsonConverterFactory.create(gson))
         .build()
 
--- API/API/app/src/main/java/ipt/dam/api/ui/activity/NoteListActivity.kt
+++ API/API/app/src/main/java/ipt/dam/api/ui/activity/NoteListActivity.kt
@@ -10,24 +10,44 @@ import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import ipt.dam.api.R
 import ipt.dam.api.model.APIResult
 import ipt.dam.api.model.Note
+import ipt.dam.api.model.TokenJWT
 import ipt.dam.api.retrofit.RetrofitInitializer
 import ipt.dam.api.ui.adapter.NoteListAdapter
+import okhttp3.Credentials
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import kotlin.random.Random
 
 class NoteListActivity : AppCompatActivity() {
+
+    lateinit var tokenJWT:String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_note_list)
 
-        listNotes()
+        val buttonAdd: Button = findViewById(R.id.button)
+        buttonAdd.setOnClickListener { addDummyNote() }
 
-        val button: Button = findViewById(R.id.button)
-        button.setOnClickListener {
-            addDummyNote()
-        }
+        val btnGetNotes: Button = findViewById(R.id.btnGetNotes)
+        btnGetNotes.setOnClickListener { listNotes() }
+
+        val btnGetNotesBA: Button = findViewById(R.id.btnGetNotesBA)
+        btnGetNotesBA.setOnClickListener { listNotesBA() }
+
+        val btnGetNotesJWT: Button = findViewById(R.id.btnGetNotesJWT)
+        btnGetNotesJWT.setOnClickListener { listNotesBA() }
+
+        val btnLoginJWT: Button = findViewById(R.id.btnLoginJWT)
+        btnLoginJWT.setOnClickListener { loginJWT() }
+
+        val btnGetNotesJWT2: Button = findViewById(R.id.btnGetNotesJWT2)
+        btnGetNotesJWT2.setOnClickListener { listNotesJWT2() }
+
+
+
+        val btnClear: Button = findViewById(R.id.btnClear)
+        btnClear.setOnClickListener { configureList(emptyList()) }
 
     }
 
@@ -41,6 +61,66 @@ class NoteListActivity : AppCompatActivity() {
     private fun listNotes() {
 
         val call = RetrofitInitializer().noteService().list()
+        processList(call)
+
+    }
+
+    private fun listNotesBA() {
+        val call = RetrofitInitializer().noteService().listBA( Credentials.basic("admin", "admin"))
+        processList(call)
+    }
+
+    private fun listNotesJWT() {
+        loginJWT("admin", "admin") {
+            Toast.makeText(this,"Token " + it?.token,Toast.LENGTH_SHORT).show()
+
+            val call = RetrofitInitializer().noteService().listJWT(token = "Bearer "+it?.token);
+            processList(call)
+        }
+    }
+
+    private fun listNotesJWT2() {
+
+            val call = RetrofitInitializer().noteService().listJWT(token = "Bearer "+tokenJWT);
+            processList(call)
+
+    }
+
+    private fun loginJWT() {
+        val call = RetrofitInitializer().noteService().loginJWT("admin","admin")
+        call.enqueue(
+            object : Callback<TokenJWT> {
+                override fun onFailure(call: Call<TokenJWT>, t: Throwable) {
+                    t.printStackTrace()
+                    Log.e("Erro","Erro no login")
+                }
+                override fun onResponse( call: Call<TokenJWT>, response: Response<TokenJWT>) {
+
+                    tokenJWT = response.body()?.token.toString()
+                    Log.i("INFO",tokenJWT)
+                }
+            }
+        )
+    }
+
+    private fun loginJWT(username: String, password: String,  onResult: (TokenJWT?) -> Unit){
+        val call = RetrofitInitializer().noteService().loginJWT(username, password)
+        call.enqueue(
+            object : Callback<TokenJWT> {
+                override fun onFailure(call: Call<TokenJWT>, t: Throwable) {
+                    t.printStackTrace()
+                    onResult(null)
+                }
+                override fun onResponse( call: Call<TokenJWT>, response: Response<TokenJWT>) {
+                    val tokenResult = response.body()
+                    onResult(tokenResult)
+                }
+            }
+        )
+    }
+
+
+    private fun processList(call: Call<List<Note>>) {
         call.enqueue(object : Callback<List<Note>?> {
             override fun onResponse(call: Call<List<Note>?>?,
                                     response: Response<List<Note>?>?) {
@@ -49,12 +129,11 @@ class NoteListActivity : AppCompatActivity() {
                     configureList(notes)
                 }
             }
-
             override fun onFailure(call: Call<List<Note>?>?, t: Throwable?) {
+                t?.printStackTrace()
                 t?.message?.let { Log.e("onFailure error", it) }
             }
         })
-
     }
 
     fun addDummyNote() {
